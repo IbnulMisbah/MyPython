4d8ea2a8b0e294349d3c9f11b84bab9c24890211
IbnulMisbah <abdullahibnulmisbah@gmail.com>
2025-01-06 00:25:11 +0600

    From Termux

--- 00Bismillah.py
+++
@@ -1,9 +0,0 @@
-# first Python code in Codium .
-
-print ('Bismillahir Rahmanir Rahim')
-
-print ('Bismillahir Rahmanir Rahim')
-
-print ("Assalamu Alaikum")
-
-print ("Assalamu Alaikum")
---
+++ 00Bismillahn.py
@@ -0,0 +1,18 @@
+<<<<<<< HEAD
+# first Python code in Codium .
+
+print ('Bismillahir Rahmanir Rahim')
+
+print ('Bismillahir Rahmanir Rahim')
+
+print ("Assalamu Alaikum")
+
+print ("Assalamu Alaikum")
+=======
+# Bismillahir Rahmanir Rahim ..
+# Its my first Python program by Neovim
+
+print ("Bismillahir Rahmanir Rahim ..")
+print ("\t")
+
+>>>>>>> f94ad62 (From Termux)
